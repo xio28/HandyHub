0b4c65c3f99b22ce879299ccb44a6eaff9f70129
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2023-05-09 00:34:49 +0100

    Update all.

--- src/Document/UsersDocument.php
+++ src/Document/UsersDocument.php
@@ -43,6 +43,35 @@ class UsersDocument {
      * @MongoDB\ReferenceOne(targetDocument="Category", storeAs="id")
      */
     private $category;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $image;
+
+    /**
+     * Get the image path associated with the user.
+     *
+     * @return string|null The image path or null if not set.
+     */
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+    
+    /**
+     * Set the image path for the user.
+     *
+     * @param string|null $image The image path or null to unset it.
+     * @return self Returns the current UsersDocument instance for method chaining.
+     */
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+    
 }
 
 ?>
