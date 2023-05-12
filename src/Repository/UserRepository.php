65d1fbd525a7b28855c0d6f028272498a31dd30c
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2023-05-12 21:57:32 +0100

    Fixed .env whitespace.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,13 +8,20 @@ use Psr\Log\LoggerInterface;
 
 class UserRepository extends DocumentRepository
 {
-    
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function getAll(): ?array
     {
         try {
             return $this->findAll();
         }  catch (MongoDBException $e) {
-            $logger->error('Error al interactuar con MongoDB: '.$e->getMessage());
+            $logger->error('Internal error while interacting with MongoDB: '.$e->getMessage());
         }
     }
 
@@ -23,32 +30,48 @@ class UserRepository extends DocumentRepository
         try {
             return $this->find($id);
         }  catch (MongoDBException $e) {
-            $logger->error('Error al interactuar con MongoDB: '.$e->getMessage());
+            $logger->error('Internal error while interacting with MongoDB: '.$e->getMessage());
         }
     }
 
     public function findUserByEmail(string $email): ?UsersDocument
     {
-        return $this->findOneBy(['email' => $email]);
+        try {
+            return $this->findOneBy(['email' => $email]);
+        }  catch (MongoDBException $e) {
+            $logger->error('Internal error while interacting with MongoDB: '.$e->getMessage());
+        }
     }
 
     public function getIdByEmail(string $email): ?string
     {
-        $user = $this->findUserByEmail($email);
+        try {
+            $user = $this->findUserByEmail($email);
 
-        return $user ? $user->getId() : null;
+            return $user ? $user->getId() : null;
+        }  catch (MongoDBException $e) {
+            $logger->error('Internal error while interacting with MongoDB: '.$e->getMessage());
+        }
     }
 
     public function getRoleByEmail(string $email): ?string
     {
-        $user = $this->findUserByEmail($email);
+        try {
+            $user = $this->findUserByEmail($email);
 
-        return $user ? $user->getRole() : null;
+            return $user ? $user->getRole() : null;
+        }  catch (MongoDBException $e) {
+            $logger->error('Internal error while interacting with MongoDB: '.$e->getMessage());
+        }
     }
 
     public function getUsersByRole(string $role): ?array
     {
-        return $this->findBy(['role' => $role]);
+        try {
+            return $this->findBy(['role' => $role]);
+        }  catch (MongoDBException $e) {
+            $logger->error('Internal error while interacting with MongoDB: '.$e->getMessage());
+        }
     }
 }
 
